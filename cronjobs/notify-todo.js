af5716360d09363f249ed64ab05cfeaf15c9aa2b
Modinat1 <adenikem@yahoo.com>
2025-11-26 13:35:22 +0100

    notification sent time adjusted

--- cronjobs/notify-todo.js
+++ cronjobs/notify-todo.js
@@ -5,8 +5,12 @@ const sendPushNotification = require("../utils/sendNotification.js");
 function notifyTodo() {
   cron.schedule("*/1 * * * *", async () => {
     const now = new Date();
-    const thirtySecBefore = new Date(now.getTime() - 30 * 1000);
-    const thirtySecAfter = new Date(now.getTime() + 30 * 1000);
+
+    // We want dueAt to be ~1 minute from now
+    const oneMinFromNow = new Date(now.getTime() + 60 * 1000);
+
+    const thirtySecBefore = new Date(oneMinFromNow.getTime() - 30 * 1000);
+    const thirtySecAfter = new Date(oneMinFromNow.getTime() + 30 * 1000);
 
     try {
       const upcomingTodos = await todoModel
@@ -20,18 +24,18 @@ function notifyTodo() {
         })
         .populate("userId", "userName pushToken");
 
-      console.log(`[${new Date().toISOString()}] Checking for reminders...`);
-      console.log(`Found ${upcomingTodos.length} upcoming todo(s)`);
+      console.log(
+        `[${now.toISOString()}] Checking reminders for todos due around ${oneMinFromNow.toISOString()}`
+      );
 
       for (const todo of upcomingTodos) {
         const user = todo.userId;
-
         if (!user?.pushToken) continue;
 
         await sendPushNotification({
           to: user.pushToken,
           title: "⏰ Todo Reminder",
-          body: `Your todo "${todo.todoTitle}" is due soon!`,
+          body: `Your todo "${todo.todoTitle}" is due in 1 minute!`,
           data: { todoId: todo._id.toString() },
         });
 
